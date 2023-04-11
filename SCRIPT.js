52dc1e7f713349e09f17882a970fe493ba4f0c02
GustavoIsihi <57574812+gustavoisihi@users.noreply.github.com>
2023-04-11 15:34:34 -0300

    Add files via upload

---
+++ SCRIPT.js
@@ -0,0 +1,38 @@
+/*for (let i = 0; i < 9; i++) {
+    document.write('<br>' + i);
+  }
+
+  for (let i = 1; i < 10; i += 2) {
+    if (i === 7) {
+      break;
+    }
+    document.write('<br>Número total de elefantes: ' + i);
+  }
+
+  const arr = [ 1, 2, 3 ];
+
+  for (let i = 0; i <= arr.length; i++) {
+    document.write('<br>'+arr[i]);
+  }
+
+  function soma(valor1,valor2){
+    return valor1 * valor2;
+}
+
+var total = soma(10,3);
+document.write('<br>'+total);
+
+function alertaHello(){
+    document.write('Olá Mundo!')
+}
+
+const carro = {
+    marca: "chevrolet",
+    modelo: "camaro",
+    ano: "2018",
+    placa: "ABC-1234",
+    descricao: function() {
+        return "<br>A marca é " + this.marca + " e o modelo é " + this.modelo
+    }
+}
+document.write(carro.descricao());*/
